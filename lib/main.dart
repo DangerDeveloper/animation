81e03c121a15f782c45c11e4b432ac04bcd0870a
dangerdeveloper <ajaysingh56656@gmail.com>
2020-02-26 23:55:57 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:animation/my_app.dart';
+
+void main() => runApp(MyApp());
---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:animation/main_page.dart';
+import 'package:animation/styles.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: mainBlack,
+      ),
+      home: MainPage(),
+    );
+  }
+}
